f9b2500568aff922cf71155824ccfdad4e0000a8
karpagalakshmi.p <karpagalakshmippandian@gmail.com>
2020-09-03 19:44:36 +0530

    commit v1

---
+++ app1.js
@@ -0,0 +1,37 @@
+(function () {
+	'use strict';
+	
+	angular.module('filApp', [])
+	.controller('filController', filController)
+	.filter('likes',LikesFilter);
+	.filter('likesall',LikesAllFilter);
+	
+	filController.$inject = ['$scope', 'likesFilter'];
+	function filController($scope,likesFilter) {
+		
+		$scope.sayMessage = function () {
+			var msg = "karpagalakshmi p";
+			return msg;
+		};
+		$scope.sayLikesMessage = function () {
+			var msg = "karpagalakshmi p";
+			msg = likesFilter(msg);
+			return msg;
+		};
+	}
+	function LikesFilter() {
+		return function (input) {
+			input = input || "";
+			input = input.replace(" ",".");
+			return input;
+		};
+	}
+	function LikesAllFilter() {
+		return function (input,target,replace) {
+			input = input || "";
+			input = input.replace(target,replace);
+			return input;
+		};
+	}
+}}();
+		
